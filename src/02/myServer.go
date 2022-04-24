4210f180bf4d020eb0682be8bfe995fd0badbd06
EliotCao <484900039@qq.com>
2022-04-24 22:27:20 +0800

    add myServer

---
+++ src/02/myServer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type MyHandler struct {}
+
+func (m *MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "by MyHandler: " + request.URL.Path)
+}
+
+func main() {
+	myhandler := MyHandler{}
+	server := http.Server{
+		Addr: ":8081",
+		Handler: &myhandler,
+		ReadTimeout: 1 * time.Second,
+	}
+	server.ListenAndServe()
+}
