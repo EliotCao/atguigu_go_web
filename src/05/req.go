9dad39f67bd1700679ac549ecfaf33f70e2585fb
EliotCao <484900039@qq.com>
2022-04-27 21:19:40 +0800

    Add request form

--- src/05/req.go
+++ src/05/req.go
@@ -9,6 +9,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "path ", r.URL.Path)
 	fmt.Fprintln(w, "RawPath ", r.URL.RawPath)
 	fmt.Fprintln(w, "RawQuery ", r.URL.RawQuery)
+	fmt.Fprintln(w, "Header", r.Header)
+	fmt.Fprintln(w, "Accept-encoding by [] ", r.Header["Accept-Encoding"])
+	fmt.Fprintln(w, "Accept-encoding by get() ", r.Header.Get("Accept-Encoding"))
+	fmt.Fprintln(w, "ContentLength ", r.ContentLength)
+	//len := r.ContentLength
+	//data := make([]byte, len)
+	//r.Body.Read(data)
+	//fmt.Fprintln(w, "body ", data)
+
+	err := r.ParseForm()
+	fmt.Println(err)
+	fmt.Fprintln(w, "请求参数", r.Form)
+	fmt.Fprintln(w, "请求参数", r.PostForm)
 }
 
 func main() {
