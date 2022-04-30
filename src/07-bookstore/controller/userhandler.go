2b3a6dfb6f157acb6b368ac749ae91bc5843cc3e
EliotCao <484900039@qq.com>
2022-04-30 09:41:33 +0800

    Add register

--- src/07-bookstore/controller/userhandler.go
+++ src/07-bookstore/controller/userhandler.go
@@ -11,10 +11,15 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	username := r.PostFormValue("username")
 	password := r.PostFormValue("password")
 	user, _ := dao.CheckUsernameAndPassword(username, password)
-	if user != nil {
-		t := template.Must(template.ParseFiles("C:\\Users\\RZNQGT\\Desktop\\atguigu_go_web\\src\\07-bookstore\\views\\pages\\user\\login.html"))
+	if user.ID > 0 {
+		t := template.Must(template.ParseFiles("C:\\Users\\RZNQGT\\Desktop\\atguigu_go_web\\src\\07-bookstore\\views\\pages\\user\\login_success.html"))
 		t.Execute(w, "")
 	}else {
-
+		t := template.Must(template.ParseFiles("C:\\Users\\RZNQGT\\Desktop\\atguigu_go_web\\src\\07-bookstore\\views\\pages\\user\\login.html"))
+		t.Execute(w, "")
 	}
+}
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	
 }
