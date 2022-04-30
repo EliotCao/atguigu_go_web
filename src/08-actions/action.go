bf1c6a5323363abdba5e1b61c323099da55ab88f
EliotCao <484900039@qq.com>
2022-04-30 16:26:14 +0800

    Add actions range

--- src/08-actions/action.go
+++ src/08-actions/action.go
@@ -7,7 +7,7 @@ import (
 )
 
 func testIf(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("C:\\Users\\RZNQGT\\Desktop\\atguigu_go_web\\src\\08-actions\\index.html"))
+	t := template.Must(template.ParseFiles("C:\\Users\\RZNQGT\\Desktop\\atguigu_go_web\\src\\08-actions\\if.html"))
 	age := 17
 	t.Execute(w, age > 18)
 }
