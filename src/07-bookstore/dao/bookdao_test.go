d7256f3ca8de44ea22c1dc5f3517489890452864
EliotCao <484900039@qq.com>
2022-05-01 15:21:44 +0800

    Add bookdao test

--- src/07-bookstore/dao/bookdao_test.go
+++ src/07-bookstore/dao/bookdao_test.go
@@ -1,1 +1,18 @@
 package dao
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBook(t *testing.T) {
+	t.Run("book", TestGetBooks)
+}
+
+func TestGetBooks(t *testing.T) {
+	books, _ := GetBooks()
+	for k, v := range books {
+		fmt.Printf("第%v个图书是%v\n", k+1, v)
+	}
+}
+
