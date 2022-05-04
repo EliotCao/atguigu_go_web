2fb8967dcb7f91e55a92e661c70fc652db791115
EliotCao <484900039@qq.com>
2022-05-04 10:13:38 +0800

    Add dao AddSession

---
+++ src/07-bookstore/dao/sessiondao.go
@@ -0,0 +1,16 @@
+package dao
+
+import (
+	"atguigu_go_web/src/04/util"
+	"atguigu_go_web/src/07-bookstore/model"
+	"path/filepath"
+)
+
+func AddSession(s *model.Session) error {
+	sqlStr := "insert into session values(?,?,?)"
+	_, err := util.Db.Exec(sqlStr, s.SessionID, s.Username, s.UserID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
